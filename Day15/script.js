d7bfb46b524c3039c93991d1f164b0e262412bb6
Hari Thapa <harithapa4654@gmail.com>
2026-02-02 18:36:51 +0530

    day 22

---
+++ Day15/script.js
@@ -0,0 +1,42 @@
+console.log("Connected!");
+console.log("script1 loaded");
+
+console.log(2 + 2);
+
+alert("Hi");
+
+let userName = prompt("Your name?");
+console.log("User name:", userName);
+
+let city = "Bhopal";
+console.log(city);
+var a = "Hari";
+let b = "Sheryians";
+const c = "School";
+
+console.log(a, b, c);
+
+a = "Updated";
+b = "Updated";
+
+
+console.log(a, b);
+
+{
+  let blockVar = "Inside block";
+  console.log(blockVar);
+}
+const PI = 3.14;
+const baseURL = "https://google.com";
+const MAX_USERS = 100;
+console.log("Name: Hari");
+console.info("Age: 20");
+console.warn("City: Bhopal");
+
+let age = prompt("Enter age:");
+console.log(typeof age);
+console.log(age + 5);
+
+let msg = "I love Sheryians";
+
+console.log(msg.slice(2, 6));

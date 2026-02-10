8d66c7c5c8da7ca3218c77e4011cbf5b0303ebb7
Hari Thapa <harithapa4654@gmail.com>
2026-02-10 14:02:58 +0530

    different color

---
+++ Day19a/script.js
@@ -0,0 +1,16 @@
+let main = document.querySelector('main')
+let box = document.getElementById('box')
+let btn = document.querySelector('button')
+
+
+btn.addEventListener('click' , changeColor)
+
+
+function changeColor(){
+    let c1 = Math.floor(Math.random()*256);
+    let c2 = Math.floor(Math.random()*256);
+    let c3 = Math.floor(Math.random()*256);
+
+    box.style.backgroundColor = `rgb(${c1} , ${c2} , ${c3}`
+    main.style.backgroundColor = `rgb(${c3} , ${c1} , ${c2}`
+}

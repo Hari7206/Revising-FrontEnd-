4ca55e2c04af541bde5a5841f8f915fe94ad2ade
Hari Thapa <harithapa4654@gmail.com>
2026-02-16 17:54:37 +0530

    day 52 and 53

--- Day25/script.js
+++ Day25/script.js
@@ -1,5 +1,6 @@
 let main = document.querySelector('main')
 let heading = document.getElementById('heading')
+let button = document.getElementById('btn')
 
 let text = heading.innerText;
 let alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -11,8 +12,17 @@ main.addEventListener('mousemove'  , function(e) {
 
    let iteration = 0
    let id = null
-heading.addEventListener('mouseenter'  , function() {
- clearInterval(id)
+
+
+heading.addEventListener('mouseenter', function() {
+    runMatrix("Click button to revael the msg ");
+    setTimeout(()=> {
+        runMatrix('Secret Message')
+    } , 6000)
+});
+
+function runMatrix(text) {
+     clearInterval(id)
   iteration = 0    
     id =  setInterval(() => {
 let str = text.split('').map((char , idx) =>{
@@ -31,4 +41,26 @@ iteration += 0.2;
         }
     } , 30)
     
-})
+}
+button.addEventListener("click", function() {
+
+   runMatrix("Revealing message...");
+
+   setTimeout(() => {
+      runMatrix("Loading resources...");
+   }, 4000);
+
+   setTimeout(() => {
+      runMatrix("Fetching data...");
+   }, 9000);
+
+   setTimeout(() => {
+      runMatrix("Decrypting content...");
+   }, 14000);
+
+   setTimeout(() => {
+      runMatrix("Secret Message");
+   }, 19000);
+
+});
+

8c674655bc28c8f3afd9045814302d37c040d83b
Hari Thapa <harithapa4654@gmail.com>
2026-03-03 14:41:18 +0530

    export/import

---
+++ day29/detail.js
@@ -0,0 +1,15 @@
+// this goona be the default export data
+let Hari = {
+    name: 'Hari Thapa',
+    age: '22',
+    ph: '92344 *****'
+}
+export default Hari;
+
+
+// this gonna be the name default
+export let Vikas = {
+    name: 'Vikas Thapa',
+    age: '16',
+    ph: '76251 *****'
+}
---
+++ day29/script.js
@@ -0,0 +1,10 @@
+import {Vikas} from "./detail.js";
+import Bigbro from './detail.js'
+
+console.log(Bigbro.name);
+
+console.log(Vikas.name);
+
+
+
+
